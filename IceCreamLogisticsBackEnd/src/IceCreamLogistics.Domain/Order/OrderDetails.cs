1bb766bd233111a6d80fbca08cf1d51c2d0f67f4
DSzondy <szondy.daniel@gmail.com>
2021-11-30 22:33:34 +0100

    Fix namespaces

--- IceCreamLogisticsBackEnd/src/IceCreamLogistics.Domain/Order/OrderDetails.cs
+++ IceCreamLogisticsBackEnd/src/IceCreamLogistics.Domain/Order/OrderDetails.cs
@@ -1,34 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using IceCreamLogistics.Domain;
 
-public class OrderDetails
+namespace IceCreamLogistics.Domain
 {
-    public int Id { get; set; }
-    public IEnumerable<OrderDetailsItem> Items { get; set; }
-    public Client Client { get; set; }
-    public DateTime OrderCreated { get; set; }
-    public DateTime RequestedDate { get; set; }
-    public OrderState OrderState { get; set; }
-    public IEnumerable<AssociatedBatch> AssociatedBatches { get; set; }
-}
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        public IEnumerable<OrderDetailsItem> Items { get; set; }
+        public Client Client { get; set; }
+        public DateTime OrderCreated { get; set; }
+        public DateTime RequestedDate { get; set; }
+        public OrderState OrderState { get; set; }
+        public IEnumerable<AssociatedBatch> AssociatedBatches { get; set; }
+    }
 
-public class OrderDetailsItem
-{
-    public Recipe Recipe { get; set; }
-    public decimal Amount { get; set; }
-    public decimal SelectedMixingAmount { get; set; }
-    public decimal MixedAmount { get; set; }
-    public decimal CancelledAmount { get; set; }
-    public decimal PendingAmount { get; set; }
-}
+    public class OrderDetailsItem
+    {
+        public Recipe Recipe { get; set; }
+        public decimal Amount { get; set; }
+        public decimal SelectedMixingAmount { get; set; }
+        public decimal MixedAmount { get; set; }
+        public decimal CancelledAmount { get; set; }
+        public decimal PendingAmount { get; set; }
+    }
 
-public class AssociatedBatch
-{
-    public int MixingBatchId { get; set; }
-    public bool BatchCompleted { get; set; }
-    public string Name { get; set; }
-    public DateTime Created { get; set; }
+    public class AssociatedBatch
+    {
+        public int MixingBatchId { get; set; }
+        public bool BatchCompleted { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
 
-    public IEnumerable<MixingBatchItem> Items { get; set; }
+        public IEnumerable<MixingBatchItem> Items { get; set; }
+    }
 }
--- IceCreamLogisticsBackEnd/src/IceCreamLogistics.Presentation/DTOs/Delivery/DeliveryDetailsDto.cs
+++ IceCreamLogisticsBackEnd/src/IceCreamLogistics.Presentation/DTOs/Delivery/DeliveryDetailsDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using IceCreamLogistics.Domain;
 
 namespace IceCreamLogistics.Presentation.DTOs
 {
